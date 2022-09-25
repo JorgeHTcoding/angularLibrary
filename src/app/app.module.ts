460a61a490facb0a81a922c75e0a696a06ff1212
JorgeHTcoding <jht.coding@gmail.com>
2022-09-25 13:28:51 +0200

    Despliegue en Heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarjetaLibroComponent } from './component/tarjeta-libro/tarjeta-libro.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetaLibroComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/tarjeta-libro/tarjeta-libro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-libro',
+  templateUrl: './tarjeta-libro.component.html',
+  styleUrls: ['./tarjeta-libro.component.css']
+})
+export class TarjetaLibroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
