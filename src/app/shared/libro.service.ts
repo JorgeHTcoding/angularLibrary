d4943ae5294fb35a0b309001dc96eec4bf6ec82a
JorgeHTcoding <jht.coding@gmail.com>
2022-09-29 13:51:18 +0200

    Cambio de endpoints despliegue heroku

--- src/app/shared/libro.service.ts
+++ src/app/shared/libro.service.ts
@@ -8,7 +8,7 @@ import { Libro } from '../Models/libro';
 })
 export class LibroService {
 
-  public url:string = "http://localhost:3000";
+  public url:string = "https://api-tek-library.herokuapp.com";
   public libro!:Libro;
 
   constructor(public http:HttpClient) { }
@@ -23,34 +23,34 @@ export class LibroService {
   }  
 
   getLibroSeleccionado(titulo:string):Observable<Object>{
-    this.url="http://localhost:3000/libro";
+    this.url="https://api-tek-library.herokuapp.com/libro";
     return this.http.post<string>(this.url,titulo);
   }
   // Obtenemos todos los libros
 
   getAll():Observable<Object>{
-    this.url="http://localhost:3000/libro";
+    this.url="https://api-tek-library.herokuapp.com/libro";
     return this.http.get(this.url);
   }
   
   // Creamos una entrada de libro
 
   postOne(libro:Libro):Observable<Object>{
-    this.url="http://localhost:3000/libro";
+    this.url="https://api-tek-library.herokuapp.com/libro";
     return this.http.post<Libro>(this.url,libro);
   }
 
   // Editamos una entrada de libro
 
   editOne(libro:Libro):Observable<Object>{
-    this.url="http://localhost:3000/libro";
+    this.url="https://api-tek-library.herokuapp.com/libro";
     return this.http.put<Libro>(this.url,libro);
   }
 
   // Eliminamos una entrada de libro con su id
 
   deleteOne(id_libro:number):Observable<Object>{
-    this.url="http://localhost:3000/libro?id_libro=" + id_libro;
+    this.url="https://api-tek-library.herokuapp.com/libro?id_libro=" + id_libro;
     return this.http.delete(this.url);
   }
 
